258ad74f77a1af9f4d0dba7602460d51873e8f8b
Francis Li <mail@francisli.com>
2020-04-08 21:06:15 -0700

    Remove unnecessary/redundant config

--- server/urls.py
+++ server/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path("", include("home.urls")),
     path("admin/", admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
