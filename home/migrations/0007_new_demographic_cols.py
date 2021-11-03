ee8bbed2333ec4ff742d0bfe1a16f4e7de35a8c3
lee51 <5643285+lee51@users.noreply.github.com>
2021-11-03 03:18:27 +0000

    New demographic info (#51)
    * Add new user demographic columns to Account
    
    * Add migration for is_sf_resident and update user api
    
    * Add default auto field

---
+++ home/migrations/0007_new_demographic_cols.py
@@ -0,0 +1,68 @@
+from django.db import migrations, models
+from home.models.account import SAN_FRANCISCO_ZIP_CODES
+
+"""
+This migration adds new demographic fields into Account:
+ * is_latino: Latino/Hispanic origin
+ * is_sf_resident: whether the user is a SF resident or not (based on zip)
+
+It also creates a new table of Race (many-to-many relationship of users and races)
+"""
+
+def populate_is_sf_resident(apps, schema_editor):
+    Account = apps.get_model("home", "Account")
+    db_alias = schema_editor.connection.alias
+
+    for account in Account.objects.using(db_alias):
+        account.is_sf_resident = account.zip in SAN_FRANCISCO_ZIP_CODES
+        account.save()
+
+def depopulate_is_sf_resident(apps, schema_editor):
+    Account = apps.get_model("home", "Account")
+    db_alias = schema_editor.connection.alias
+
+    for account in Account.objects.using(db_alias):
+        account.is_sf_resident = None
+        account.save()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('home', '0006_auto_20200623_2207'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Race',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('label', models.CharField(max_length=75, unique=True)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='account',
+            name='is_sf_resident',
+            field=models.BooleanField(help_text='Whether the user is a SF resident or not, based on zip', null=True),
+        ),
+        migrations.AddField(
+            model_name='account',
+            name='is_latino',
+            field=models.BooleanField(blank=True, help_text='Latino or Hispanic origin', null=True),
+        ),
+        migrations.AddField(
+            model_name='account',
+            name='race',
+            field=models.ManyToManyField(blank=True, help_text='Self-identified race(s) of user', to='home.Race'),
+        ),
+        migrations.AddField(
+            model_name='account',
+            name='gender',
+            field=models.CharField(blank=True, help_text='Self-identified gender identity of user', max_length=25),
+        ),
+        migrations.AddField(
+            model_name='account',
+            name='is_tester',
+            field=models.BooleanField(help_text='User is an app tester', default=False),
+        ),
+        migrations.RunPython(populate_is_sf_resident, reverse_code=depopulate_is_sf_resident),
+    ]
--- home/models/account.py
+++ home/models/account.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+SAN_FRANCISCO_ZIP_CODES = set([
+    '94102', '94103', '94104', '94105', '94107', '94108', '94109', '94110',
+    '94111', '94112', '94114', '94115', '94116', '94117', '94118', '94121',
+    '94122', '94123', '94124', '94127', '94129', '94130', '94131', '94132',
+    '94133', '94134', '94158',
+])
 
 # Note: Maybe inherit from Django's User model?
 class Account(models.Model):
@@ -13,6 +19,12 @@ class Account(models.Model):
     name = models.CharField(max_length=250, help_text="User's name")
     zip = models.CharField(max_length=25, help_text="User's zipcode")
     age = models.IntegerField(help_text="User's age")
+    is_sf_resident = models.BooleanField(null=True, help_text="Whether the user is a SF resident or not, based on zip")
+    is_latino = models.BooleanField(null=True, blank=True, help_text="Latino or Hispanic origin")
+    race = models.ManyToManyField("Race", blank=True, help_text="Self-identified race(s) of user")
+    gender = models.CharField(max_length=25, blank=True, help_text="Self-identified gender identity of user")
+
+    is_tester = models.BooleanField(default=False, help_text="User is an app tester")
     contests = models.ManyToManyField("Contest", blank=True, help_text="All the contests the account has enrolled in")
     created = models.DateTimeField(auto_now_add=True, help_text="Accounts creation timestamp")
     updated = models.DateTimeField(auto_now=True, help_text="Accounts updation timestamp")
