6504136b13faa24350feae817747d25806f49d05
Ed Lee <leefiftyone@gmail.com>
2021-12-15 23:31:34 +0000

    Add env variable DEPLOY_ENV and check in resetdb

--- home/management/commands/resetdb.py
+++ home/management/commands/resetdb.py
@@ -1,4 +1,6 @@
 import csv
+import os
+import sys
 
 from django.core.management.base import BaseCommand
 from django.db import connection
@@ -8,6 +10,10 @@ class Command(BaseCommand):
     help = "Reset database (truncate application tables)"
 
     def handle(self, *args, **options):
+        if os.getenv("DEPLOY_ENV").lower().startswith("prod"):
+            print("This command is not allowed on a production server.")
+            sys.exit(1)
+
         question = "\nAre you sure you want to truncate all tables? (Type `yes` exactly): "
         answer = input(question)
         if answer == "yes":
@@ -18,7 +24,9 @@ class Command(BaseCommand):
             cursor.execute("TRUNCATE TABLE home_account RESTART IDENTITY CASCADE;")
             print("All accounts, devices, and walks have been reset.")
 
-            # TODO: Also reset contests?
+            # Also truncate contests
+            cursor.execute("TRUNCATE TABLE home_contest CASCADE;")
+            print("All contests have been reset.")
 
         else:
             print("Aborted resetdb.\n")
