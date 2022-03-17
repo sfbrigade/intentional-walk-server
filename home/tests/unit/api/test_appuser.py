e5a85dce7a224da33081d8059abeee7d32be7c56
emdat <datta.emon@gmail.com>
2022-03-16 18:48:56 -0700

    Add sexual orientation to account. Add decline to answer enum values. Convert is_latino to enum to allow decline to answer as response.

--- home/tests/unit/api/test_appuser.py
+++ home/tests/unit/api/test_appuser.py
@@ -22,9 +22,10 @@ class TestIsTester(TestCase):
 class TestValidateAccountInput(TestCase):
     def test_valid_input(self):
         examples = [
-            dict(zip="12345", age=99, is_latino=True, race=["BL"], gender="TF"),
+            dict(zip="12345", age=99, is_latino="YE", race=["BL"], gender="TF", sexual_orien="SG"),
+            dict(zip="12345", age=99, is_latino="DA", race=["DA"], gender="DA"),
             dict(is_latino=None, gender=None, race=None),
-            dict(gender="OT", gender_other="other gender"),
+            dict(gender="OT", gender_other="other gender", sexual_orien="OT", sexual_orien_other="pansexual"),
             dict(race=["BL", "OT"], race_other="other race"),
             dict(),
         ]
@@ -37,8 +38,9 @@ class TestValidateAccountInput(TestCase):
             dict(name=""),
             dict(zip="1234"),
             dict(age=0),
-            dict(is_latino="yes"),
+            dict(is_latino=True),
             dict(gender="", gender_other="other gender"),
+            dict(sexual_orien="", sexual_orien_other="idk"),
             dict(race=None, race_other="other race"),
             dict(gender="NB", gender_other="nonbinary"),
         ]
