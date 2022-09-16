205c67d6ee6c8eef0f5384715104b5fa21ae5101
Kevin Morris <kevr@0cost.org>
2022-09-15 17:39:46 -0700

    fix(home.utils.localize): support datetime input
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- home/utils/__init__.py
+++ home/utils/__init__.py
@@ -1,4 +1,5 @@
 from datetime import date, datetime
+from typing import Union
 
 from django.utils import timezone
 
@@ -6,7 +7,9 @@ from django.utils import timezone
 # This function converts a date into a datetime based on the default time zone
 # (TIME_ZONE, which set in server/settings.py but overridden in .env).
 # This is particularly useful for comparing dates to datetimes in db queries.
-def localize(d: date) -> datetime:
-    dt = datetime.combine(d, datetime.min.time())
+def localize(d: Union[date, datetime]) -> datetime:
+    dt = d
+    if isinstance(d, date):
+        dt = datetime.combine(d, datetime.min.time())
     tz = timezone.get_default_timezone()
-    return tz.localize(dt)
+    return dt.astimezone(tz)
