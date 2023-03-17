af55a5152e5668524767204d154ff560090a2747
Ben <benjamin.perlman@gmail.com>
2023-03-16 18:09:12 -0700

    commenting out unfinished testing file

--- home/tests/unit/api/test_leaderboard.py
+++ home/tests/unit/api/test_leaderboard.py
@@ -1,28 +1,28 @@
 from django.test import Client, TestCase
 
-import json
-import requests
+# import json
+# import requests
 from models import Device
 from datetime import datetime
 
-for x in range(30):
-    testd = Device.objects.values("device_id").order_by("?").first()
-    post_data = {
-        "account_id": testd["device_id"],
-        "daily_walks": [
-            {
-                "date": datetime.today().strftime("%Y-%m-%d"),
-                "steps": 0,
-                "distance": 0,
-            }
-        ],
-    }
+# for x in range(30):
+#     testd = Device.objects.values("device_id").order_by("?").first()
+#     post_data = {
+#         "account_id": testd["device_id"],
+#         "daily_walks": [
+#             {
+#                 "date": datetime.today().strftime("%Y-%m-%d"),
+#                 "steps": 0,
+#                 "distance": 0,
+#             }
+#         ],
+#     }
 
-    response = requests.post(
-        "http://localhost:8000/api/dailywalk/create",
-        data=json.dumps(post_data),
-    )
-    content = response.content
+    # response = requests.post(
+    #     "http://localhost:8000/api/dailywalk/create",
+    #     data=json.dumps(post_data),
+    # )
+    # content = response.content
 
 
 #  real_response = {
