5375d796cfbaf65e73058141e2b1266198ab36da
odo-sfct <atlantis.wong+sfct@gmail.com>
2024-04-30 20:47:18 -0700

    Update proxy to target to 127.0.0.1:8000

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     ["/admin", "/api", "/static/home"],
     createProxyMiddleware({
-      target: "http://localhost:8000",
+      target: "http://127.0.0.1:8000",
       changeOrigin: true,
     })
   );
