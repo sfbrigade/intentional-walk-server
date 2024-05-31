e8a55c3dac180577aab980a2fa8c37ce6ac821a6
Patrick Yu <patrickgod1@aol.com>
2024-05-30 20:18:51 -0700

    Fix loading css and js files on new admin login page

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    ["/admin", "/api", "/static/home"],
+    ["/admin", "/api", "/static/home", "/static/admin"],
     createProxyMiddleware({
       target: "http://127.0.0.1:8000",
       changeOrigin: true,
