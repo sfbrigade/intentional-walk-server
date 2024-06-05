9409bc006dc3286aa326fb4ba5ec1cb72731f142
Patrick <60832092+patrickgods1@users.noreply.github.com>
2024-06-04 23:42:50 -0700

    Update server/settings.py
    Added check if dev environment
    
    Co-authored-by: odo-sfct <161795448+odo-sfct@users.noreply.github.com>

--- server/settings.py
+++ server/settings.py
@@ -180,5 +180,6 @@ LOGGING = {
 
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
-# Add trusted origins
-CSRF_TRUSTED_ORIGINS = [f"http://localhost:3000"]
+# React client is on 3000, while django admin auth is on 8000
+if os.getenv("DEPLOY_ENV") == "development":
+   CSRF_TRUSTED_ORIGINS = ["http://localhost:3000", "http://127.0.0.1:3000"]
