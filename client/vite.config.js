4689ed6313d491334bb00d835f3e217c1609afb7
Francis Li <mail@francisli.com>
2025-01-08 20:35:19 -0800

    [Closes #277] Configure poetry for dependency management only. Proxy Django admin.

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
+      '/admin': 'http://localhost:8000',
       '/api': 'http://localhost:8000',
       '/static': 'http://localhost:8000'
     }
